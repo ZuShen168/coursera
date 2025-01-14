d66e95af9e6ef98c7458d19edec00b4a1c0b0809
Ng Zu Shen <ngzushen@gmail.com>
2025-01-15 05:25:30 +0800

    course 2 module 1 complete

--- course 2/module 1.js
+++ course 2/module 1.js
@@ -1,1 +1,32 @@
-console.log('Hello World')
+// console.log('Hello World');
+
+// var petDog = "Rex";
+// var petCat = "Pepper";
+// console.log(petDog);
+// console.log(petCat);
+// console.log("My pet dog's name is: ", petDog);
+// console.log("My pet cat's name is: ", petCat);
+// 
+// var catSound = "purr";
+// var dogSound = "woof";
+// console.log(petDog, "says", dogSound);
+// console.log(petCat, "says", catSound);
+// 
+// catSound = "meow";
+// console.log(petCat, "now says", catSound);
+
+for (var i = 1; i <= 10; i++){
+    switch (i){
+        case 1:
+            console.log("Gold medal");
+            break;
+        case 2:
+            console.log("Silver medal");
+            break;
+        case 3:
+            console.log("Bronze medal");
+            break;
+        default:
+            console.log(i)
+    }
+}
